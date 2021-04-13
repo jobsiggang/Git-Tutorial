f7a2b67262f9549affdbda73811fc415c06989bf
jobsiggang <jjchaser29@gmail.com>
2021-04-13 19:59:34 +0900

    4월13일

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-print("hello PYthon")
+print("hello PYthon on git")
 for number in range(2,10):
     for n in range(1,10):
         print(number,n,number*n)
        # print("{} {} {}").format(number, n,number*n)
+#version3
+
        
